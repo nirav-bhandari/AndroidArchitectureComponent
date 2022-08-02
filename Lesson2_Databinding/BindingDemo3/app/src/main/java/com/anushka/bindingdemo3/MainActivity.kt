6994cacd3c39a25070d1eb4221666e27e5fc9533
Nirav Bhandari <nbhandari@rylabs.io>
2022-08-03 01:38:40 +0530

    clear :q :wq asd

--- Lesson2_Databinding/BindingDemo3/app/src/main/java/com/anushka/bindingdemo3/MainActivity.kt
+++ Lesson2_Databinding/BindingDemo3/app/src/main/java/com/anushka/bindingdemo3/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.anushka.bindingdemo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.anushka.bindingdemo3.databinding.ActivityMainBinding
 
@@ -9,13 +9,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        val student =getStudent()
-        binding.nameText.text = student.name
-        binding.emailText.text = student.email
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = getStudent()
     }
 
-    private fun getStudent():Student{
-        return Student(1,"Alex","alex@gmail.com")
+    private fun getStudent(): Student {
+        return Student(1, "Alex", "alex@gmail.com")
     }
 }
